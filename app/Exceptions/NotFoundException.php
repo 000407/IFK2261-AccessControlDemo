e53ee3d24bd7cce33458f17405e28657b83b9537
Kanchana <kanchana@senadheera.net>
2023-09-26 10:50:16 +0530

    Bug fixes

--- app/Exceptions/NotFoundException.php
+++ app/Exceptions/NotFoundException.php
@@ -6,6 +6,6 @@ use RuntimeException;
 
 class NotFoundException extends RuntimeException {
   public static function new(String $resourceName, String $id = null) {
-    return new NotFoundException("No $resourceName was found for $id.", 404);
+    return new NotFoundException("No $resourceName was found for ID $id.", 404);
   }
 }
--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -14,34 +14,16 @@ class ProductService {
   public function __construct(SessionManager $session) {
     $this->session = $session;
   }
-  
-  public function upsertLocationStock($productId, $locationId, $quantity) {
-    $product = Product::find($productId);
-
-    if ($product != null) {
-      if (Location::exists($locationId)) {
-        $product->locations()->syncWithPivotValues($locationId, [
-          'quantity' => $quantity,
-        ], false);
-      } else {
-        $status = 404;
-        $message = "No location found for ID $locationId";
-      }
-    } else {
-      $status = 404;
-      $message = "No product found for ID $productId";
-    }
-  }
 
   public function upsertLocationStock($productId, $locationId, $quantity) {
     $product = Product::find($productId);
 
     if ($product == null) {
-      throw new NotFoundException("PRODUCT", $productId);
+      throw NotFoundException::new("PRODUCT", $productId);
     }
 
-    if (!Location::exists($locationId)) {
-      throw new NotFoundException("LOCATION", $locationId);
+    if (!Location::where('id', $locationId )->exists()) {
+      throw NotFoundException::new("LOCATION", $locationId);
     }
 
     $product->locations()->syncWithPivotValues($locationId, [
